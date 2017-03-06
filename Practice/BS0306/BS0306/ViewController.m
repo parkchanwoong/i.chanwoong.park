402c98a90326f9d92c60c82491e42e74fa1d88df
박찬웅 <foodi1121@gmail.com>
2017-03-06 20:48:25 +0900

    공부~

--- Practice/BS0306/BS0306/ViewController.m
+++ Practice/BS0306/BS0306/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tv;
+@property (weak, nonatomic) IBOutlet UITableViewCell *cell;
 @end
 
 @implementation ViewController
@@ -17,6 +19,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    self.tv.delegate = self;
+    self.tv.dataSource = self;
     
 }
 
@@ -26,5 +32,22 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [self.tv dequeueReusableCellWithIdentifier:@"cell"];
+    
+//    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return 600;
+}
 @end
