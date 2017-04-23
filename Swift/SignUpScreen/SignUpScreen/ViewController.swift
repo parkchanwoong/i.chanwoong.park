d064f4a60df20d0268b174f0a5e7c00cc5be494e
Chanwoong Park <foodi1121@gmail.com>
2017-04-24 01:17:24 +0900

    Add SignUpScreen

--- Swift/SignUpScreen/SignUpScreen/ViewController.swift
+++ Swift/SignUpScreen/SignUpScreen/ViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController,UITextFieldDelegate {
+    //딜리게이트 메서드 일정 상황이 되면 자동으로 호출되는 메서드
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var idTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
@@ -18,11 +18,28 @@ class ViewController: UIViewController {
     
     @IBOutlet var resultTextView: UITextView!
     
-    @IBAction func signUpAction(_ sender: AnyObject) {
+    @IBAction func signUpAction(_ sender: AnyObject)
+    {
+        resultTextView.text = "\(nameTextField.text!) 님 가입을 축하합니다."
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+//        print("textFieldShouldReturn")
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        self.view.endEditing(true)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        nameTextField.delegate = self //네임텍스트필드에 대한 이벤트를 나 자신에게 던져주겠다.
     }
 
     override func didReceiveMemoryWarning() {
