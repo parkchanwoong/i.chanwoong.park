5091e2f31eef4cdc38607ebfefb4068bb596bd4e
Chanwoong Park <foodi1121@gmail.com>
2017-05-11 16:46:38 +0900

    Add TabbarEx

---
+++ Swift/TabbarEx/TabbarEx/ThirdViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ThirdViewController.swift
+//  TabbarEx
+//
+//  Created by 박찬웅 on 2017. 5. 11..
+//  Copyright © 2017년 Parkchanwoong. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    @IBOutlet var resultLabel: UILabel!
+    @IBAction func buttonAction(_ sender: Any)
+    {
+        resultLabel.text = "3번 눌림"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
